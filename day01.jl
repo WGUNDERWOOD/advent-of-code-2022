afdd7a017c4420cf24c6f37ff0b9e65d6871f264
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2022-12-01 16:23:43 -0500

    day 01 completed

--- day01.jl
+++ day01.jl
@@ -1,1 +1,22 @@
+open("day01.txt") do file
 
+    counter = 0
+    totals = Int[]
+
+    for l in eachline(file)
+        if l != ""
+            counter += parse(Int, l)
+        else
+            push!(totals, counter)
+            counter = 0
+        end
+    end
+    push!(totals, counter)
+
+    # part 1
+    display(maximum(totals))
+
+    # part 2
+    top_3_totals = sort(totals, rev=true)[1:3]
+    display(sum(top_3_totals))
+end
