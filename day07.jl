3aef158c6e7abe807d3b060b095e151d55e22da6
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2022-12-12 15:55:52 -0500

    stuck

--- day07.jl
+++ day07.jl
@@ -1,6 +1,6 @@
 #println("Day 7")
 
-struct Directory
+mutable struct Directory
     name::String
     size::Real
     directories::Vector{String}
@@ -11,15 +11,24 @@ end
 struct File
     name::String
     size::Int
-    parent::Directory
+    parent::String
 end
 
 mutable struct Filesystem
-    structure::Dict
+    structure::Dict{String, Union{File, Directory}}
     cwd::String
 end
 
-function ls!(files::Vector{File}, directories::Vector{Directory}, fs::Filesystem)
+function add_file!(name::String, size::Int, fs::Filesystem)
+    full_name = fs.cwd * "/" * name
+    full_name = replace(full_name, "//" => "/")
+    fs.structure[full_name] = File(full_name, size, fs.cwd)
+end
+
+function add_directory!(name::String, fs::Filesystem)
+    full_name = fs.cwd * "/" * name
+    full_name = replace(full_name, "//" => "/")
+    fs.structure[full_name] = Directory(full_name, 0, String[], String[], fs.cwd)
 end
 
 function cd!(name::String, fs::Filesystem)
@@ -37,61 +46,83 @@ function cd!(name::String, fs::Filesystem)
         new_cwd = old_cwd * "/" * name
         fs.cwd = replace(new_cwd, "//" => "/")
 
-        if !(fs.cwd in keys(fs.structure))
-            fs.structure[fs.cwd] = Directory(fs.cwd, NaN, String[], String[], nothing)
-        end
-
         return fs
     end
 
     error("Cannot cd: subdirectory not found")
 end
 
-function chunk_output(output::Vector(String))
-end
 
-#function ls(cwd::Directory, fs::Dict)
+function propagate_sizes!(fs::Filesystem)
 
-#end
+    depth = maximum([count(x -> (x == '/'), o.name) for o in values(fs.structure)])
 
-fs = Filesystem(Dict("/" => Directory("/", NaN, String[], String[], nothing)), "/")
-#cd!("/", fs)
-#ls!([Directory("jmtrrrp", NaN, String[], String[], nothing)], [File("")] fs)
-
-
-#cd!("jmtrrrp", fs)
-
-
-
-f = readlines("day07.txt")
-
-for i in 1:10
-    l = f[i]
-    println(l)
-    l_split = split(l, " ")
-
-    if l_split[1] == "\$"
-
-        if l_split[2] == "cd"
-            println("do cd stuff")
-            name = String(l_split[3])
-            cd!(name, fs)
-
-        elseif l_split[2] == "ls"
-            println("do ls stuff")
-
-            for j in 1:10
-                new_l = f[i+j]
-                new_l_split = split(new_l, " ")
-                if new_l_split[1] != "\$"
-                    println("ls output")
-                    println(new_l)
-                end
+    for d in 1:depth
+        for k in keys(fs.structure)
+            dir = fs.structure[k]
+            if isa(dir, Directory)
+                dir.size += sum([fs.structure[d].size for d in dir.directories])
+                dir.size += sum([fs.structure[f].size for f in dir.files])
             end
         end
     end
 end
 
+
+function large_directories(threshold::Int, fs::Filesystem)
+
+    large_dirs = Directory[]
+
+    for k in keys(fs.structure)
+        object = fs.structure[k]
+        if isa(object, Directory)
+            if object.size >= threshold
+                push!(large_dirs, object)
+            end
+        end
+    end
+
+    return large_dirs
+end
+
+
+
+f = readlines("day07.txt")
+fs = Filesystem(Dict("/" => Directory("/", NaN, String[], String[], nothing)), "/")
+
+for l in f
+
+    println(l)
+    l_split = split(l, " ")
+
+    if l_split[1] == "\$" && l_split[2] == "cd"
+        name = String(l_split[3])
+        cd!(name, fs)
+
+    elseif l_split[1] != "\$"
+
+        if l_split[1] == "dir"
+            name = String(l_split[2])
+            add_directory!(name, fs)
+
+        else
+            name = String(l_split[2])
+            size = parse(Int, l_split[1])
+            add_file!(name, size, fs)
+
+        end
+    end
+end
+
+#display(fs)
+#println(length(fs.structure))
+
+propagate_sizes!(fs)
+large = large_directories(100000, fs)
+display(large)
+println(sum([d.size for d in large]))
+
+#display(fs.structure)
 
 #println("Part 1: ", first_n_all_different(f, 4))
 #println("Part 2: ", first_n_all_different(f, 14))
