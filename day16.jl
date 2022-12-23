bf853d26a9773f47bec0aa622b15efa4d754a399
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2022-12-23 17:25:58 +0000

    little better?

--- day16.jl
+++ day16.jl
@@ -1,63 +1,62 @@
-using Combinatorics
+using DataStructures
 
 
-mutable struct State
-    opens::Dict{String, Bool}
-    time::Int
-    pressure::Int
-    position::String
-end
-
-
-Valves = Dict{String, Int}
-Tunnels = Dict{Tuple{String, String}, Int}
-
-
-function Base.copy(state::State)
-    return State(copy(state.opens), state.time, state.pressure, state.position)
+struct State
+    opens::Set{UInt8}
+    time::UInt16
+    pressure::UInt16
+    position::UInt8
 end
 
 
 function parse_input(filepath::String)
 
     file = readlines(filepath)
-    valves = Valves()
-    opens = Dict{String, Bool}()
-    tunnels = Tunnels()
+    n = length(file)
+    id_to_index = Dict{String, UInt8}()
 
-    for l in file
+    for i in 1:n
+        l = file[i]
         split_l = String.(split(l, [' ', ';', ',', '='], keepempty=false))
         id = split_l[2]
+        id_to_index[id] = i
+    end
+
+    valves = Vector{UInt8}(undef, n)
+    opens = Set{UInt8}()
+    tunnels = Matrix{UInt8}(undef, n, n)
+    tunnels .= 0
+
+    for i in 1:n
+        l = file[i]
+        split_l = String.(split(l, [' ', ';', ',', '='], keepempty=false))
         flow = parse(Int, split_l[6])
         open = false
-        valves[id] = flow
-        opens[id] = open
+        valves[i] = flow
 
         for dest in split_l[11:end]
-            tunnels[id, dest] = 1
+            tunnels[i, id_to_index[dest]] = 1
         end
     end
 
     time = 0
     pressure = 0
-    position = "AA"
-    state = State(opens, time, pressure, position)
+    AA_position = id_to_index["AA"]
+    state = State(opens, time, pressure, AA_position)
 
-    return (valves, tunnels, state)
+    return (valves, tunnels, state, AA_position)
 end
 
 
-function show(state::State)
+function Base.show(state::State)
 
     println("Time: ", state.time)
     println("Pressure released: ", state.pressure)
     println("Current position: ", state.position)
 
     print("Open valves: ")
-    for k in keys(state.opens)
-        if state.opens[k]
-            print(k, " ")
-        end
+    for k in state.opens
+        print(k, " ")
     end
     println()
 
@@ -65,44 +64,48 @@ function show(state::State)
 end
 
 
-function shortest_path_lengths(source::String, valves::Valves, tunnels::Tunnels)
+function shortest_path_lengths(source::UInt8, valves::Vector{UInt8}, tunnels::Matrix{UInt8})
 
-    ks = keys(valves)
-    visited = Dict{String, Bool}(ks .=> false)
-    lens = Dict(ks .=> Inf)
+    n = length(valves)
+    visited = fill(false, n)
+    lens = fill(Inf, n)
     lens[source] = 0
     terminated = false
 
     while !terminated
 
-        min_unvis_len = minimum([lens[k] for k in ks if !visited[k]])
-        current = [k for k in ks if lens[k] == min_unvis_len && !visited[k]][begin]
+        min_unvis_len = minimum([lens[i] for i in 1:n if !visited[i]])
+        current = findfirst(i -> (lens[i] == min_unvis_len && !visited[i]), 1:n)
 
-        for (v1, v2) in keys(tunnels)
-            if v1 == current
-                len = tunnels[(v1, v2)]
-                lens[v2] = min(lens[v1] + len, lens[v2])
+        for v in 1:n
+            if v != current
+                len = tunnels[current, v]
+                if len > 0
+                    lens[v] = min(lens[current] + len, lens[v])
+                end
             end
         end
 
         visited[current] = true
-        terminated = all(values(visited))
+        terminated = all(visited)
     end
 
     return lens
 end
 
 
-function complete(valves::Valves, tunnels::Tunnels)
+function complete(valves::Vector{UInt8}, tunnels::Matrix{UInt8})
 
-    ks = keys(valves)
-    new_tunnels = Tunnels()
+    n = length(valves)
+    new_tunnels = Matrix{UInt8}(undef, n, n)
 
-    for v1 in ks
-        lens = shortest_path_lengths(v1, valves, tunnels)
-        for v2 in ks
+    for v1 in 1:n
+        lens = shortest_path_lengths(UInt8(v1), valves, tunnels)
+        for v2 in 1:n
             if v1 != v2
-                new_tunnels[(v1, v2)] = lens[v2]
+                new_tunnels[v1, v2] = lens[v2]
+            else
+                new_tunnels[v1, v2] = UInt8(0)
             end
         end
     end
@@ -111,22 +114,21 @@ function complete(valves::Valves, tunnels::Tunnels)
 end
 
 
-function remove_zero_valves(valves::Valves, tunnels::Tunnels)
+function remove_zero_valves(valves::Vector{UInt8}, tunnels::Matrix{UInt8}, AA_position::UInt8)
 
-    ks = keys(valves)
-    non_zeros = [k for k in ks if valves[k] != 0 || k == "AA"]
-    new_valves = Valves()
-    new_tunnels = Tunnels()
+    n = length(valves)
+    non_zeros = [i for i in 1:n if valves[i] != 0 || i == AA_position]
+    nnz = length(non_zeros)
+    new_valves = Vector{UInt8}(undef, nnz)
+    new_tunnels = Matrix{UInt8}(undef, nnz, nnz)
 
-    for k in non_zeros
-        new_valves[k] = valves[k]
+    for i in 1:nnz
+        new_valves[i] = valves[non_zeros[i]]
     end
 
-    for k1 in non_zeros
-        for k2 in non_zeros
-            if haskey(tunnels, (k1, k2))
-                new_tunnels[(k1, k2)] = tunnels[(k1, k2)]
-            end
+    for i1 in 1:nnz
+        for i2 in 1:nnz
+            new_tunnels[i1, i2] = tunnels[non_zeros[i1], non_zeros[i2]]
         end
     end
 
@@ -134,78 +136,88 @@ function remove_zero_valves(valves::Valves, tunnels::Tunnels)
 end
 
 
-function move(position, valves, tunnels, state)
+function move(position::UInt8, valves::Vector{UInt8}, tunnels::Matrix{UInt8}, state::State)
 
-    new_state = copy(state)
+    # TODO use remaining time/pressure instead?
+    n = length(valves)
 
     if position == state.position
-        new_state.time += 1
-        new_state.pressure += sum([valves[k] for k in ks if state.opens[k]])
-        new_state.opens[position] = true
+        time = state.time + 1
+        pressure = state.pressure + sum([valves[i] for i in 1:n if i in state.opens])
 
     else
-        len = tunnels[(state.position, position)]
-        new_state.time += len + 1
-        new_state.pressure += sum([valves[k] for k in ks if state.opens[k]]) * (len + 1)
-        new_state.position = position
-        new_state.opens[position] = true
-
+        len = tunnels[state.position, position]
+        time = state.time + len + 1
+        pressure = state.pressure + sum([valves[i] for i in 1:n if i in state.opens]) * (len + 1)
     end
 
-    return new_state
+    opens = copy(state.opens)
+    push!(opens, position)
+
+    return State(opens, time, pressure, position)
 end
 
 
-function total_pressure(limit::Int, valves::Valves, state::State)
+function total_pressure(limit::Int, valves::Vector{UInt8}, state::State)
 
-    total_flow = sum([valves[k] for k in keys(valves) if state.opens[k]])
+    n = length(valves)
+    total_flow = sum([valves[i] for i in 1:n if i in state.opens])
     return state.pressure + (limit - state.time) * total_flow
 end
 
 
 
 # load and process input
-(valves, tunnels, state) = parse_input("day16.txt")
-#(valves, tunnels, state) = parse_input("day16test.txt")
+(valves, tunnels, state, AA_position) = parse_input("day16.txt")
+#(valves, tunnels, state, AA_position) = parse_input("day16test.txt")
 tunnels = complete(valves, tunnels)
-(valves, tunnels) = remove_zero_valves(valves, tunnels)
-limit = 30
-ks = keys(valves)
+(valves, tunnels) = remove_zero_valves(valves, tunnels, AA_position)
 
-checking = State[state]
-checked = State[]
+n = length(valves)
+limit = 30
+checking = Deque{State}()
+push!(checking, state)
+best_score = 0
 
 rep = 0
 
+#for rep in 1:10000
 while !isempty(checking)
 
     global rep += 1
 
-    old_state = pop!(checking)
+    old_state = popfirst!(checking)
 
-    for position in [k for k in ks if !old_state.opens[k]]
+    for position in [UInt8(i) for i in 1:n if !(i in old_state.opens)]
         new_state = move(position, valves, tunnels, old_state)
 
         if new_state.time <= limit
-            if all([new_state.opens[k] for k in ks])
-                push!(checked, new_state)
-            else
-                push!(checking, new_state)
+            push!(checking, new_state)
+            new_score = total_pressure(limit, valves, new_state)
+            if new_score > best_score
+                global best_score = new_score
+                show(new_state)
+                println(best_score)
             end
         end
     end
 
-    println(rep)
-    println(length(checking))
-    show(checking[end])
+    if rep % 1000 == 0
+        #println(rep)
+        #println(best_score)
+        #println(length(checking))
+        #println(maximum([sum(values(s.opens)) for s in checking]))
+        #println()
+    end
+    #show.(checking)
+    #show.(checked)
     #println()
-    show.(checked)
 end
 
 #show.(checking)
 #println(length(checking))
-#println(maximum([sum(values(s.opens)) for s in checked]))
-println(maximum([total_pressure(limit, valves, state) for state in checked]))
+#println(maximum([total_pressure(limit, valves, state) for state in checked]))
+println(best_score)
 
 
 
