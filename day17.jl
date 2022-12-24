668e0d627a2b85bcefa03f425d505c38823d6690
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2022-12-24 00:25:45 +0000

    day 17 part 1

--- day17.jl
+++ day17.jl
@@ -4,6 +4,7 @@ mutable struct Chamber
     jet_ind::Int
     const rocks::Vector{Matrix{Char}}
     rock_ind::Int
+    n_rocks::Int
 end
 
 
@@ -28,8 +29,9 @@ function parse_input(filepath::String)
     rocks = Matrix{Char}[rock1, rock2, rock3, rock4, rock5]
 
     rock_ind = 1
+    n_rocks = 0
 
-    return Chamber(tower, jets, jet_ind, rocks, rock_ind)
+    return Chamber(tower, jets, jet_ind, rocks, rock_ind, n_rocks)
 
 end
 
@@ -70,6 +72,9 @@ function iterate!(chamber::Chamber)
     if !('@' in chamber.tower)
         adjust_height!(chamber)
         spawn_rock!(chamber)
+    else
+        apply_jet!(chamber)
+        apply_fall!(chamber)
     end
 
 end
@@ -105,15 +110,67 @@ function spawn_rock!(chamber::Chamber)
     for i in 1:size(rock, 1)
         for j in 1:size(rock, 2)
             if rock[i,j] == '#'
-                chamber.tower[i, j+3] = '@'
+                chamber.tower[i+4-size(rock, 1), j+3] = '@'
             end
         end
+    end
+
+    chamber.rock_ind = 1 + (chamber.rock_ind % length(chamber.rocks))
+    chamber.n_rocks += 1
+    return nothing
+end
+
+
+function apply_jet!(chamber::Chamber)
+
+    at_locs = Tuple.(findall(x -> x == '@', chamber.tower))
+    jet = chamber.jets[chamber.jet_ind]
+
+    jet == '>' ? dir = 1 : dir = -1
+    if all([chamber.tower[i, j + dir] in ['.', '@'] for (i, j) in at_locs])
+        for loc in at_locs; chamber.tower[loc[1], loc[2]] = '.'; end
+        for loc in at_locs; chamber.tower[loc[1], loc[2] + dir] = '@'; end
+    end
+
+    chamber.jet_ind = 1 + (chamber.jet_ind % length(chamber.jets))
+    return nothing
+end
+
+
+function apply_fall!(chamber::Chamber)
+
+    at_locs = Tuple.(findall(x -> x == '@', chamber.tower))
+
+    if all([chamber.tower[i+1, j] in ['.', '@'] for (i, j) in at_locs])
+        for loc in at_locs; chamber.tower[loc[1], loc[2]] = '.'; end
+        for loc in at_locs; chamber.tower[loc[1]+1, loc[2]] = '@'; end
+    else
+        for loc in at_locs; chamber.tower[loc[1], loc[2]] = '#'; end
     end
 
     return nothing
 end
 
+
+function get_height(chamber::Chamber)
+
+    hash_locs = Tuple.(findall(x -> x == '#', chamber.tower))
+    top_hash = minimum(loc[1] for loc in hash_locs)
+    return size(chamber.tower, 1) - top_hash
+end
+
+
+# part 1
 chamber = parse_input("day17.txt")
-show(chamber)
-iterate!(chamber)
-show(chamber)
+#chamber = parse_input("day17test.txt")
+n_rocks = 0
+
+while n_rocks <= 2022
+    iterate!(chamber)
+    global n_rocks = chamber.n_rocks
+end
+
+height = get_height(chamber)
+println(height)
+
+# part 2
