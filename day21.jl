9c73e00e3e1088cbac1cce161aeb52d6949a4759
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2022-12-28 22:08:00 +0000

    day 21

--- day21.jl
+++ day21.jl
@@ -2,8 +2,8 @@ println("Day 21")
 
 mutable struct Monkey
     id::String
-    expr::Union{Vector{String}, Nothing}
-    value::Union{Int, Nothing}
+    expr::Union{String, Tuple{String, String, String}}
+    parsed::Bool
 end
 
 
@@ -17,70 +17,101 @@ function parse_input(filepath::String)
         id = split_l[1]
 
         if all(s in '0':'9' for s in split_l[3])
-            expr = nothing
-            value = parse(Int, split_l[3])
+            expr = split_l[3]
+            parsed = true
         else
-            expr = split_l[3:5]
-            value = nothing
+            expr = Tuple(split_l[3:5])
+            parsed = false
         end
 
-        monkey = Monkey(id, expr, value)
-        push!(monkeys, monkey.id => monkey)
+        monkey = Monkey(id, expr, parsed)
+        push!(monkeys, id => monkey)
     end
 
     return monkeys
 end
 
 
-function update!(id::String, monkeys::Dict{String, Monkey})
+function update_expr!(id::String, monkeys::Dict{String, Monkey})
 
-    if isnothing(monkeys[id].value)
-        a = monkeys[id].expr[1]
-        op = monkeys[id].expr[2]
-        b = monkeys[id].expr[3]
-        a_val = monkeys[a].value
-        b_val = monkeys[b].value
+    if !monkeys[id].parsed
+        a_id = monkeys[id].expr[1]
+        b_id = monkeys[id].expr[3]
 
-        if !isnothing(a_val) && !isnothing(b_val)
-            if op == "+"
-                monkeys[id].value = a_val + b_val
-            elseif op == "-"
-                monkeys[id].value = a_val - b_val
-            elseif op == "*"
-                monkeys[id].value = a_val * b_val
-            elseif op == "/"
-                monkeys[id].value = a_val / b_val
-            end
+        if monkeys[a_id].parsed && monkeys[b_id].parsed
+            a_expr = "(" * join(monkeys[a_id].expr) * ")"
+            b_expr = "(" * join(monkeys[b_id].expr) * ")"
+            op = monkeys[id].expr[2]
+            monkeys[id].expr = (a_expr, op, b_expr)
+            monkeys[id].parsed = true
         end
     end
 end
 
 
-#monkeys = parse_input("day21test.txt")
-monkeys = parse_input("day21.txt")
-
-
 # part 1
+monkeys = parse_input("day21.txt")
 new_monkeys = deepcopy(monkeys)
 terminated = false
 
 while !terminated
     for id in keys(new_monkeys)
-        update!(id, new_monkeys)
+        update_expr!(id, new_monkeys)
     end
-    global terminated = all(!isnothing(monkey.value) for monkey in values(new_monkeys))
+    global terminated = all(monkey.parsed for monkey in values(new_monkeys))
 end
 
-println(new_monkeys["root"].value)
+answer = round(Int, eval(Meta.parse(join(new_monkeys["root"].expr))))
+println("Part 1: ", answer)
+
 
 # part 2
-monkeys["humn"] = Monkey("humn", ["humn", "+", "0"], nothing)
+new_monkeys = deepcopy(monkeys)
+terminated = false
+new_monkeys["humn"] = Monkey("humn", "humn", true)
 
-for rep in 1:10000
+while !terminated
     for id in keys(new_monkeys)
-        update!(id, new_monkeys)
+        update_expr!(id, new_monkeys)
+    end
+    global terminated = all(monkey.parsed for monkey in values(new_monkeys))
+end
+
+if 'h' in new_monkeys["root"].expr[1]
+    arrow = Meta.parse(new_monkeys["root"].expr[1])
+    arrow = @eval (humn) -> $arrow
+    target = round(Int, eval(Meta.parse(new_monkeys["root"].expr[3])))
+else
+    arrow = Meta.parse(new_monkeys["root"].expr[3])
+    arrow = @eval (humn) -> $arrow
+    target = round(Int, eval(Meta.parse(new_monkeys["root"].expr[1])))
+end
+
+# initial guesses
+lo = -1
+hi = 1
+
+# expand binary search interval
+while !(min(arrow(lo), arrow(hi)) <= target <= max(arrow(lo), arrow(hi)))
+    global lo *= 2
+    global hi *= 2
+end
+
+# run binary search on interval
+while hi - lo > 1
+    mid = div(lo + hi, 2)
+    if min(arrow(lo), arrow(mid)) <= target <= max(arrow(lo), arrow(mid))
+        global hi = mid
+    else
+        global lo = mid
     end
 end
-display(monkeys)
 
-#println(new_monkeys["root"].value)
+# print answers
+if arrow(lo) == target
+    answer = lo
+elseif arrow(hi) == target
+    answer = hi
+end
+
+println("Part 2: ", answer)
